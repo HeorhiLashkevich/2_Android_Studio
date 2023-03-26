8871d10597d36e48737b2ea1f2943cd2e4b6c0a9
Lashkevich Heorhi <cobvscof@gmail.com>
2023-03-26 22:04:22 +0300

    2.2

--- app/src/main/java/com/example/hw2/MainActivity.kt
+++ app/src/main/java/com/example/hw2/MainActivity.kt
@@ -1,29 +1,43 @@
 package com.example.hw2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var text: TextView
+    private lateinit var editText: EditText
     private lateinit var button: Button
     private var countOfHello = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val b = Bundle()
-        val i = Intent(this@MainActivity, HelloActivity::class.java)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        text = findViewById(R.id.hello_counts)
-        button = findViewById(R.id.say_hello)
+        button = findViewById(R.id.count)
+        text = findViewById(R.id.counts)
+        editText = findViewById(R.id.editText)
         button.setOnClickListener {
             countOfHello++
-            b.putString("helloCount", countOfHello.toString())
-            i.putExtras(b)
-            startActivity(i)
             text.text = countOfHello.toString()
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY, countOfHello)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        countOfHello = savedInstanceState.getInt(KEY)
+        text.text = countOfHello.toString()
+    }
+
+    companion object {
+        const val KEY = "KEY"
+    }
+
 }
